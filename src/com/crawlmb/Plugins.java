87663cc72f3f49ee60cbbdb4641a3ce3686df247
michaelbarlow7 <michaelbarlow7@gmail.com>
2012-07-16 21:00:47 +1000

    Screen tapping now maps to numberpad, arrow keys map to arrow keys

--- src/com/crawlmb/Plugins.java
+++ src/com/crawlmb/Plugins.java
@@ -23,6 +23,15 @@ final public class Plugins {
 
 	static final String DEFAULT_PROFILE = "0~Default~PLAYER~0~0|0~Borg~BORGSAVE~1~1";
 	public static String LoaderLib ="loader-angband";
+	static final int KEY_A1	= 0534;		/* upper left of keypad */
+	static final int KEY_A3	= 0535;		/* upper right of keypad */
+	static final int KEY_B2	= 0536;		/* center of keypad */ //CURRENTLY NOT USED
+	static final int KEY_C1	= 0537;		/* lower left of keypad */
+	static final int KEY_C3	= 0540;		/* lower right of keypad */
+	static final int KEY_DOWN= 0402;		/* down-arrow key */
+	static final int KEY_UP	= 0403;		/* up-arrow key */
+	static final int KEY_LEFT= 0404;		/* left-arrow key */
+	static final int KEY_RIGHT= 0405;		/* right-arrow key */
 
 	public static String getFilesDir(Plugin p) {
 		switch (p) {
@@ -34,29 +43,29 @@ final public class Plugins {
 
 	public static int getKeyDown(Plugin p) {
 		switch (p) {
-		case Angband: return 0x80;
-		default: return '2';
+//		case Angband: return 0x80;
+		default: return KEY_DOWN;
 		}
 	}
 
 	public static int getKeyUp(Plugin p) {
 		switch (p) {
-		case Angband: return 0x83;
-		default: return '8';
+//		case Angband: return 0x83;
+		default: return KEY_UP;
 		}
 	}
 
 	public static int getKeyLeft(Plugin p) {
 		switch (p) {
-		case Angband: return 0x81;
-		default: return '4';
+//		case Angband: return 0x81;
+		default: return KEY_LEFT;
 		}
 	}
 
 	public static int getKeyRight(Plugin p) {
 		switch (p) {
-		case Angband: return 0x82;
-		default: return '6';
+//		case Angband: return 0x82;
+		default: return KEY_RIGHT;
 		}
 	}
 
